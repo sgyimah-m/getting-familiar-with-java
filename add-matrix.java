65006e7a980d027ef47a248f25b0c8af0a869f4a
sgyimah-m <63011309+sgyimah-m@users.noreply.github.com>
2023-04-28 16:23:05 +0000

    Add 2 Matrices
    This code performs addition of two dimensional matrices

---
+++ add-matrix.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class AddTwoMattrix {
+    public static void main (String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number of rows and cloumns of matrix");
+        m = in.nextInt();
+        n = in.nextInt();
+        int first [] [] = new int [m] [n]; int sum [] [] = new int [m] [n];
+        System.out.println("Enter the elementsof the first matrix");
+        for ( c=0;c<m;c++)
+        for ( )
+        
+    }
+}
