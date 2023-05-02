139ca19fccf22fdbc83c8b4429b8b3b585a14a5d
sgyimah-m <63011309+sgyimah-m@users.noreply.github.com>
2023-05-02 11:50:19 +0000

    local variables
    changed m and n to local variables

--- add-matrix.java
+++ add-matrix.java
@@ -3,12 +3,32 @@ class AddTwoMattrix {
     public static void main (String args[]) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the number of rows and cloumns of matrix");
-        m = in.nextInt();
-        n = in.nextInt();
-        int first [] [] = new int [m] [n]; int sum [] [] = new int [m] [n];
+        int m = in.nextInt();
+        int n = in.nextInt();
+        int first [] [] = new int [m] [n];
+        int second [] [] = new int [m] [n]; int sum [] [] = new int [m] [n];
         System.out.println("Enter the elementsof the first matrix");
-        for ( c=0;c<m;c++)
-        for ( )
+        int d;
+        for ( int c = 0 ; c < m ; c++ )
+        for ( d = 0; d < n; d++ )
+        int m,n,c,d;
+        first [c] [d] = in.nextInt();
+        System.out.println("Enter the elements of the second matrix");
+        for ( c = 0; c < m; c++ )
+        for ( d = 0; d < n; d++ )
+        second [c] [d] = in.nextInt(); for ( c = 0; c < m; c++ ) for ( d = 0; d < n; d++ )
+        sum [c] [d] = first [c] [d] + second [c] [d];
+        System.out.println("Sum of entered matrices : --");
+        for ( c = 0; c < m; c++ )
+        {
+            for ( d = 0; d < n; d++ )
+            System.out.print(sum[c] [d] + "\t");
+            System.out.println;
+        }
         
     }
+    
+
+
+      
 }
