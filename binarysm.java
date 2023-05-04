03eebc48c19b2a28a81632aeb21ce2b3ef2235e7
sgyimah-m <63011309+sgyimah-m@users.noreply.github.com>
2023-05-04 18:40:42 +0000

    Update binarysm.java

--- binarysm.java
+++ binarysm.java
@@ -1,9 +1,11 @@
 import java.util.Arrays;
-class binarysm {
+public class binarysm {
     public static void main(String args[])
     {
         char characters[] = { 'a', 'b', 'c', 'd', 'e' };
+
+        
         System.out.println(Arrays.binarySearch( characters, 'a'));
-        System.out.println(Arrays.binarySearch(characters, 'p'));
+        System.out.println(Arrays.binarySearch(characters, 'd'));
     }
 }
